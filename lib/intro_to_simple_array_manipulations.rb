fd1926c1b8143dd69449ea5b66f07e83832477cb
Kevin Garcia <kg.gar91@gmail.com>
2020-06-09 18:52:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,6 +25,7 @@ end
 def using_shift(my_favorite_cities)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   im_so_over_this_city = my_favorite_cities.shift
+  p im_so_over_this_city
 end
 
 def shift_with_args(ice_cream_brands)
